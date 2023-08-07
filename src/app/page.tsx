dc4e3abc1e8f8e0a217861cb2031cf39a0079aa3
Fitrie <mfitrie78@gmail.com>
2023-08-07 23:08:34 +0800

    add skills/abilities

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import AboutMe from "@/components/about-me";
 import MainPage from "../components/main-page";
+import SkillsAbilities from "@/components/skills-abilities";
 
 export default function Home() {
   return (
     <>
       <MainPage />
       <AboutMe />
+      <SkillsAbilities />
     </>
   )
 }
---
+++ src/components/skills-abilities.tsx
@@ -0,0 +1,56 @@
+
+export default function SkillsAbilities(){
+    return (
+        <div className="h-screen grid grid-row-6">
+            <div className="row-span-1 flex justify-center items-center bg-yellow-200">
+                <h2 className="text-4xl font-bold">
+                    Skills & Abilities
+                </h2>
+            </div>
+            <div className="row-span-5 p-4">
+                <div className="h-full p-4 grid grid-rows-5 border rounded-xl gap-5 bg-black/10">
+                    <div className="grid grid-cols-6 gap-5">
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                    </div>
+                    <div className="grid grid-cols-6 gap-5">
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                    </div>
+                    <div className="grid grid-cols-6 gap-5">
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                    </div>
+                    <div className="grid grid-cols-6 gap-5">
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                    </div>
+                    <div className="grid grid-cols-6 gap-5">
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                        <div className="border rounded-xl w-full h-full flex justify-center items-center text-white hover:shadow-2xl" style={{backgroundColor: "#192a56"}}>row</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
