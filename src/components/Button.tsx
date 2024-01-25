0073259a260bb3ed9308b545dd7ac7c60322bd5c
Fitrie <mfitrie78@gmail.com>
2024-01-25 09:18:50 +0800

    feat(aboutme): finish about me

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,12 @@
-export default function Button({ children, }: { children: React.ReactNode }){
+interface ButtonType{
+    children: React.ReactNode,
+    handleOnClick: () => void,
+}
+
+export default function Button({ children, handleOnClick }: ButtonType){
     return (
-        <button 
+        <button
+            onClick={ handleOnClick }
             className="rounded-lg px-4 py-2 flex gap-1 items-center text-white shadow-xl duration-500 ease-in-out hover:bg-green-700 hover:transition-all hover:gap-3" 
             title="About me!"
             style={{backgroundColor: '#3498db'}}
--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -1,8 +1,22 @@
+"use client"
+
+import Image from "next/image";
 import Button from "./Button";
 import ArrowRight from "./icons/arrow-right";
 import UserIcon from "./icons/user";
 
 export default function AboutMe(){
+
+    const downloadResume = () => {
+        const fileUrl = '/RESUME FITRIE.pdf';
+        const link = document.createElement('a');
+        link.href = fileUrl;
+        link.download = 'RESUME FITRIE';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+
     return (
         <div className="h-screen grid grid-rows-6">
             <div className="row-span-1 flex justify-center items-center bg-green-500">
@@ -14,20 +28,28 @@ export default function AboutMe(){
             <div className="row-span-5 grid grid-cols-2">
                 <div className="flex justify-center items-center">
                     {/* <p>Column one</p> */}
-                    <img
+                    {/* <img
                         id="me-image"
                         className="h-3/6 w-2/6 rounded-lg drop-shadow-2xl cursor-pointer grayscale hover:grayscale-0"
-                        src="https://picsum.photos/seed/picsum/300/300"
+                        // src="https://picsum.photos/seed/picsum/300/300"
+                        src="../../public/Fitrie-casual.jpg"
+                        alt="avatar-image"
+                    /> */}
+                    <Image
+                        className="h-4/6 w-2/6 rounded-lg drop-shadow-2xl cursor-pointer grayscale hover:grayscale-0"
+                        width={200}
+                        height={300}
+                        src="/Fitrie-casual.jpg"
                         alt="avatar-image"
                     />
                 </div>
                 <div className="px-8 flex flex-col justify-center gap-4">
                     <div>
-                        <p className="text-2xl font-bold">Im Fitrie</p>
+                        <p className="text-2xl font-bold">I'm Fitrie</p>
                         <p className="text-lg font-semibold">Full Stack Developer</p>
                     </div>
                     <div className="text-justify">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil ex quod accusantium! Excepturi similique repellat, neque aperiam dolorem porro temporibus, vel, quo perferendis unde consequatur minima nobis! Provident, aliquid suscipit!</p>
+                        <p>I am a diligent and versatile professional with a profound aptitude for assimilating new skills. I possess a robust foundation in JavaScript, Node.js, TypeScript, Go, and design patterns, enabling me to proficiently develop resilient applications.</p>
                     </div>
                     <div>
                         <span className="text-blue-700">Email: </span>
@@ -38,7 +60,7 @@ export default function AboutMe(){
                         <span>Klang, Malaysia - 41050</span>
                     </div>
                     <div className="mt-8">
-                        <Button>
+                        <Button handleOnClick={ downloadResume }>
                             Resume 
                             <ArrowRight size="20" hexColor="#fff"/>
                         </Button>
