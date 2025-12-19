de2712de7b765425cf13de4512f19f3d848b26bc
fitrie <mfitrie78@gmail.com>
2025-12-19 22:03:02 +0800

    feat: update code

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,34 @@
 "use client"
 
 import Background3d from "@/components/background-3d"
+import { Card, CardContent } from "@/components/ui/card"
 
 
 export default function Home() {
   return (
-    <div className="h-full w-full">
+    <div className="">
       <Background3d />
-      <div className="h-full">
-        <h1 className="scroll-m-20 text-left text-4xl font-extrabold tracking-tight text-balance text-white drop-shadow-lg">
-          Hello, I'm Fitrie
-        </h1>
+      <div className="h-[30rem]">
+        <Card>
+          {/* <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+            <CardAction>Card Action</CardAction>
+          </CardHeader> */}
+          <CardContent>
+            <h1 className="scroll-m-20 text-left text-4xl font-extrabold tracking-tight text-balance text-white drop-shadow-lg">
+              Hello, I'm Fitrie
+            </h1>
+            <span>
+              I'm passionate about building tools that make developers' lives easier. When I'm not coding, you'll find me writing about my experiences and sharing what I've learned.
+              Let's connect and build something amazing together! Whether you're looking to collaborate on a project or just want to chat about tech, I'd love to hear from you.
+            </span>
+          </CardContent>
+          {/* <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter> */}
+        </Card>
+
       </div>
     </div>
   )
